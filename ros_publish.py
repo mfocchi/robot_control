7c3325da0724593ca0fb976e49db5598d91a1205
Michelel Focchi <michele.focchi@iit.it>
2020-06-18 10:04:57 +0200

    added possibility to plot separately the visual markers

--- ros_publish.py
+++ ros_publish.py
@@ -16,23 +16,25 @@ import os
 import time as tm
 
 class RosPub():
-    def __init__(self): 
-        #launch rviz node if not yet done will start roscore too
-        uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
-        roslaunch.configure_logging(uuid)
-        self.launch = roslaunch.parent.ROSLaunchParent(uuid, [os.environ['LOCOSIM_DIR'] + "/robot_control/visualize.launch"])
-        self.launch.start()                                                    
-        ros.loginfo("RVIZ started")
-        tm.sleep(1.0)                            
+    def __init__(self, only_visual = False):
+        if (not only_visual):                    
+            #launch rviz node if not yet done will start roscore too
+            uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
+            roslaunch.configure_logging(uuid)
+            self.launch = roslaunch.parent.ROSLaunchParent(uuid, [os.environ['LOCOSIM_DIR'] + "/robot_control/visualize.launch"])
+            self.launch.start()                                                    
+            ros.loginfo("RVIZ started")
+            tm.sleep(1.0)
+            #init ros node to publish joint states and vis topics                                
+            ros.init_node('ros_pub_node', anonymous=False, log_level=ros.FATAL)
+           #set joint state publisher 
+            self.joint_pub = ros.Publisher("/joint_states", JointState, queue_size=1)                                
         print("---------------------------------------------------------------")   
-        #init ros node to publish joint states and vis topics                                
-        ros.init_node('sub_pub_node_python', anonymous=False, log_level=ros.FATAL)
-        #set joint state publisher            
-        self.joint_pub = ros.Publisher("/joint_states", JointState, queue_size=1)
-        self.marker_pub = ros.Publisher('vis' , MarkerArray, queue_size=1)                                
+       
+        self.marker_pub = ros.Publisher('/vis' , MarkerArray, queue_size=1)                                
         self.markerArray = MarkerArray()
         self.markerArray.markers = []     
-        self.id = 0	                         
+        self.id = 0                             
                                 
     def publish(self,robot, q, qd = None, tau = None):
     
@@ -50,14 +52,16 @@ class RosPub():
         msg.effort = tau              
         
         self.joint_pub.publish(msg)
-                                
+        publishVisual()                                   
+ 
+    def publishVisual(self):                                
         #publish also the markers if any
         if len(self.markerArray.markers)>0:                                                                                                                            
             self.marker_pub .publish(self.markerArray)  
         #reset the marker array making it ready for another round
         self.markerArray.markers = []                             
         self.id = 0
-								
+                                
     def add_marker(self, pos):
        marker = Marker()
        marker.header.frame_id = "world"
@@ -69,19 +73,28 @@ class RosPub():
        marker.color.a = 0.5
        marker.color.r = 1.0
        marker.color.g = 1.0
-       marker.color.b = 0.0						
-       						
+       marker.color.b = 0.0                        
+                               
        marker.pose.orientation.w = 1.0
        marker.pose.position.x = pos[0]
        marker.pose.position.y = pos[1] 
        marker.pose.position.z = pos[2] 
 
        marker.id = self.id       
-       self.id += 1										
+       self.id += 1                                        
        self.markerArray.markers.append(marker)
-							
-    def add_arrow(self, start, vector):
+                            
+    def add_arrow(self, start, vector, color = "green"):
        marker = Marker()
+       if (color == "green"):                    
+           marker.color.r = 0.0
+           marker.color.g = 1.0
+           marker.color.b = 0.0                    
+       if (color == "blue"):                    
+           marker.color.r = 0.0
+           marker.color.g = 0.0
+           marker.color.b = 1.0                                                
+
        marker.header.frame_id = "world"
        marker.type = marker.ARROW
        marker.action = marker.ADD  
@@ -91,11 +104,9 @@ class RosPub():
        marker.scale.y = 0.05
        marker.scale.z = 0.05
        marker.color.a = 1.0
-       marker.color.r = 0.0
-       marker.color.g = 1.0
-       marker.color.b = 1.0
+
        marker.id = self.id
-       self.id += 1	 
+       self.id += 1     
        self.markerArray.markers.append(marker)
                     
     def deregister_node(self):
