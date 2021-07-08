afc9c7f9d041beb3e017fa62ffb20d4840e23834
Michele Focchi <michele.focchi@iit.it>
2021-07-08 14:11:35 +0200

    added solo replay for 12 and 16 dofs

--- base_controller/solo_replay.py
+++ base_controller/solo_replay.py
@@ -17,9 +17,11 @@ os.system("killall rosmaster rviz gzserver gzclient")
 br = tf.TransformBroadcaster()
 
 
-ros_pub = RosPub("solo12") #start only graphic stuff
-q_des = np.array([-0.0, 0.7, -1.4, -0.0, -0.7, 1.4,  -0.0, 0.7, -1.4,-0.0, -0.7, 1.4])
-robot = getRobotModel("solo12")
+ros_pub = RosPub("solo") #start only graphic stuff
+robot = getRobotModel("solo", generate_urdf = True)
+q_des = np.zeros((robot.na))
+
+q_des[:12] = np.array([-0.0, 0.7, -1.4, -0.0, -0.7, 1.4,  -0.0, 0.7, -1.4,-0.0, -0.7, 1.4])
 
 translation = (0.0, 0.0, 1.0)
 rotation = (0.0, 0.0, 0.0, 1.0)
@@ -28,8 +30,14 @@ rate = rospy.Rate(5)  # 5hz
 while not rospy.is_shutdown():
     br.sendTransform(translation, rotation, Time.now(), '/base_link', '/world')
     ros_pub.publish(robot, q_des)
+    # flywheel
+    if (robot.na>12):
+        q_des[12]+=0.5
+        q_des[13]+=0.5
+        q_des[14]+=0.5
+        q_des[15]+=0.5
     rate.sleep()
-
-
+    
+    
 
     
