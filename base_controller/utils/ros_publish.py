80c6f1e015bd2f3232b11976efa2f9a45c50c1d7
froscia <Jaybird_96>
2022-02-04 18:45:17 +0100

    introduction of visual_frame + bugfix in markers

--- base_controller/utils/ros_publish.py
+++ base_controller/utils/ros_publish.py
@@ -17,9 +17,9 @@ import tf
 from rospy import Time
 
 class RosPub():
-    def __init__(self, robot_name="ur5", only_visual = False):
+    def __init__(self, robot_name="solo", only_visual = False, visual_frame = "world"):
         
-        print("Starting ros impedance controller---------------------------------------------------------------")   
+        print("Starting ros pub---------------------------------------------------------------")
         if (not only_visual):                           
             #launch rviz node if not yet done will start roscore too
             uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
@@ -28,19 +28,25 @@ class RosPub():
             self.launch.start()                                                    
             ros.loginfo("RVIZ started")
             tm.sleep(1.0)
-            #init ros node to publish joint states and vis topics                                
-            ros.init_node('ros_pub_node', anonymous=False, log_level=ros.FATAL)
-           #set joint state publisher 
-            self.joint_pub = ros.Publisher("/joint_states", JointState, queue_size=1)                                
+                               
    
-       
-        self.marker_pub = ros.Publisher('/vis' , MarkerArray, queue_size=1)                                
+        #init ros node to publish joint states and vis topics
+        ros.init_node('sub_pub_node_python', anonymous=False, log_level=ros.FATAL)
+        #set joint state publisher 
+        self.joint_pub = ros.Publisher("/joint_states", JointState, queue_size=1) 
+        self.marker_pub = ros.Publisher('/vis' , MarkerArray, queue_size=1)
+        self.arrow_pub = ros.Publisher('/arrow', MarkerArray, queue_size=1)
         self.markerArray = MarkerArray()
-        self.markerArray.markers = []     
-        self.id = 0           
+        self.markerArray.markers = []
+        self.markerArray_arrows = MarkerArray()
+        self.markerArray_arrows.markers = []
+        self.id = 0
+        self.id_arrow = 0
+
         self.broadcaster = tf.TransformBroadcaster()  
         self.fixedBaseRobot = False
-                                
+        self.visual_frame = visual_frame
+        
     def publish(self,robot, q, qd = None, tau = None):
     
         if (qd is None ):
@@ -72,29 +78,35 @@ class RosPub():
     def publishVisual(self):                                
         #publish also the markers if any
         if len(self.markerArray.markers)>0:                                                                                                                            
-            self.marker_pub.publish(self.markerArray)  
-        #reset the marker array making it ready for another round
-        self.markerArray.markers = []                             
-        self.id = 0
+            self.marker_pub.publish(self.markerArray)
+            # reset the marker array making it ready for another round
+            self.markerArray.markers = []
+            self.id = 0
+
+        if len(self.markerArray_arrows.markers) > 0:
+            self.arrow_pub.publish(self.markerArray_arrows)
+            self.markerArray_arrows.markers = []
+            self.id_arrow = 0
                                 
-    def add_marker(self, pos):
+    def add_marker(self, pos, radius = 0.1):
        marker = Marker()
-       marker.header.frame_id = "world"
+       marker.header.frame_id = self.visual_frame
        marker.type = marker.SPHERE
        marker.action = marker.ADD
-       marker.scale.x = 0.1
-       marker.scale.y = 0.1
-       marker.scale.z = 0.1
+       marker.scale.x = radius
+       marker.scale.y = radius
+       marker.scale.z = radius
        marker.color.a = 0.5
        marker.color.r = 1.0
-       marker.color.g = 1.0
+       marker.color.g = 0.0
        marker.color.b = 0.0                        
                                
        marker.pose.orientation.w = 1.0
        marker.pose.position.x = pos[0]
        marker.pose.position.y = pos[1] 
-       marker.pose.position.z = pos[2] 
-
+       marker.pose.position.z = pos[2]
+       marker.lifetime = ros.Duration(0.0)
+       
        marker.id = self.id       
        self.id += 1                                        
        self.markerArray.markers.append(marker)
@@ -110,19 +122,20 @@ class RosPub():
            marker.color.g = 0.0
            marker.color.b = 1.0                                                
 
-       marker.header.frame_id = "world"
+       marker.header.frame_id = self.visual_frame
        marker.type = marker.ARROW
-       marker.action = marker.ADD  
-       marker.points.append(Point(start[0], start[1], start[2]))    
-       marker.points.append(Point(start[0] + vector[0], start[1] + vector[1], start[2] + vector[2]))                                                                                      
+       marker.action = marker.ADD
+       marker.points.append(Point(start[0], start[1], start[2]))
+       marker.points.append(Point(start[0] + vector[0], start[1] + vector[1], start[2] + vector[2]))
        marker.scale.x = 0.02
        marker.scale.y = 0.04
        marker.scale.z = 0.02
        marker.color.a = 1.0
+       marker.lifetime = ros.Duration(0.0)
 
-       marker.id = self.id
-       self.id += 1     
-       self.markerArray.markers.append(marker)
+       marker.id = self.id_arrow
+       self.id_arrow += 1
+       self.markerArray_arrows.markers.append(marker)
        
     def add_cone(self,  origin, normal, friction_coeff, color = "green"):     
         
@@ -139,7 +152,7 @@ class RosPub():
            marker.color.g = 0.0
            marker.color.b = 1.0                                                
 
-       marker.header.frame_id = "world"
+       marker.header.frame_id = self.visual_frame
        marker.type = marker.ARROW
        marker.action = marker.ADD      
        
