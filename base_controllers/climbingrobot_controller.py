bugfix in phi computation that was always zero

base_controllers/climbingrobot_controller.py
@@ -64,12 +64,25 @@ class ClimbingrobotController(BaseControllerFixed):
 
     def rot2phi_theta(self, R):
         # this rotation is obtrained rotating phi about Z and theta about -Y axis as in Luigis convention
+        # Rsym_x = [	1   ,    0     	  ,  	  0,
+        #                 0   ,    cos(psi) ,  -sin(psi),
+        #                 0   ,    sin(psi) ,  cos(psi)];
+        # Rsym_y =[cos(theta) ,	 0  ,   sin(theta),
+        #           0       ,    1  ,   0,
+        #           -sin(theta) 	,	 0  ,  cos(theta)];
+        #
+        # Rsym_z =[cos(phi), -sin(phi), 0,
+        #           sin(phi), cos(phi), 0,
+        #           0, 0, 1];
+        # the transpose is equal to rotate of -theta about Y  which is the same as rotating theta about -Y
+        #R = simplify(Rsym_z * Rsym_y')
+
         #[cos(phi) * cos(theta), -sin(phi), -cos(phi) * sin(theta)]
         #[cos(theta) * sin(phi), cos(phi), -sin(phi) * sin(theta)]
         #[sin(theta), 0, cos(theta)]
-        phi = np.arctan2(R[1, 0], R[0, 0])  #cos(theta) * sin(phi)/cos(phi) * cos(theta)
+        # note, with the urdf we have R = Rsym_y'*Rsym_x which has R(1,0) null
+        phi = np.arctan2(-R[0, 1], R[1, 1])  #cos(theta) * sin(phi)/cos(phi) * cos(theta)
         theta = np.arctan2(R[2, 0],R[2,2])
-
         # returns  pitch = theta,  yaw = phi
         return theta, phi
 