025d804633972f546ec1cc3d332d30135ff3c4e5
Michele Focchi <michele.focchi@unitn.it>
2022-09-08 15:03:52 +0200

    added gripper functions

--- base_controllers/L8_admittance.py
+++ base_controllers/L8_admittance.py
@@ -14,7 +14,7 @@ import sys
 from docutils.nodes import label
 from geometry_msgs.msg import WrenchStamped
 from geometry_msgs.msg import Wrench, Point
-from std_srvs.srv import Trigger
+from std_srvs.srv import Trigger, TriggerRequest
 
 # ros utils
 import roslaunch
@@ -53,6 +53,67 @@ from base_controllers.utils.kin_dyn_utils import fifthOrderPolynomialTrajectory 
 
 import tf
 from rospy import Time
+
+def resend_robot_program():
+    ros.sleep(1.5)
+    ros.wait_for_service("/ur5/ur_hardware_interface/resend_robot_program")
+    sos_service = ros.ServiceProxy('/ur5/ur_hardware_interface/resend_robot_program', Trigger)
+    sos = TriggerRequest()
+    result = sos_service(sos)
+    # print(result)
+    ros.sleep(0.1)
+
+def move_gripper(diameter):
+    import os
+    import sys
+    import socket
+
+    HOST = "192.168.0.100"  # The UR IP address
+    PORT = 30002  # UR secondary client
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    sock.settimeout(0.5)
+    try:
+        sock.connect((HOST, PORT))
+    except:
+        raise Exception("Cannot connect to end-effector socket") from None
+    sock.settimeout(None)
+    scripts_path =  rospkg.RosPack().get_path(robotName+'_description') + '/grpper/scripts'
+
+    onrobot_script = scripts_path + "/onrobot_superminimal.script";
+    file = open(onrobot_script, "rb")  # Robotiq Gripper
+    lines = file.readlines()
+    file.close()
+
+    tool_index = 0
+    blocking = True
+    cmd_string = f"tfg_release({diameter},  tool_index={tool_index}, blocking={blocking})"
+
+    line_number_to_add = 446
+
+    new_lines = lines[0:line_number_to_add]
+    new_lines.insert(line_number_to_add + 1, str.encode(cmd_string))
+    new_lines += lines[line_number_to_add::]
+
+    offset = 0
+    buffer = 2024
+    file_to_send = b''.join(new_lines)
+
+    if len(file_to_send) < buffer:
+        buffer = len(file_to_send)
+    data = file_to_send[0:buffer]
+    while data:
+        sock.send(data)
+        offset += buffer
+        if len(file_to_send) < offset + buffer:
+            buffer = len(file_to_send) - offset
+        data = file_to_send[offset:offset + buffer]
+    sock.close()
+
+    print("Gripper moved, now resend robot program")
+    resend_robot_program()
+    return
+
 
 class LabAdmittanceController(BaseControllerFixed):
     
