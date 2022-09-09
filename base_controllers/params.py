78203925af68d88364f6cb8be78af1e686f269c1
Michele Focchi <michele.focchi@iit.it>
2022-09-09 17:17:47 +0200

    jump works with obstacle (0, 1.5), p0= 0.224, 0, -8], pf=[3,3,-20]

--- base_controllers/params.py
+++ base_controllers/params.py
@@ -63,7 +63,7 @@ robot_params['climbingrobot'] ={'dt': 0.001,
                        'kp': np.array([0 ,    0,    400,  100,    50,   50,    50, 30, 30]),
                        'kd':  np.array([0,    0,    10,   10,     10,   10,     4,   4, 4  ]),
 
-                       'q_0':  np.array([ 0, 0, 3.0, 0,0,0, -1.57, 0.0, 0.0 ]),
+                       'q_0':  np.array([ 0, 0, 8.0, 0,0,0, -1.57, 0.0, 0.0 ]),
                        'joint_names': ['mountain_wire_pitch', 'mountain_wire_roll',  'wire_base_prismatic',
                                        'wire_base_pitch', 'wire_base_roll','wire_base_yaw',
                                        'hip_pitch', 'hip_roll', 'knee'],
