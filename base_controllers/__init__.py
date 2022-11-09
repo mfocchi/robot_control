8f16a1cb9890162c6097a3e451eca0088cf715a3
Michele Focchi <michele.focchi@iit.it>
2022-11-09 17:45:18 +0100

    added TkAgg for new versions of pycharm

--- base_controllers/__init__.py
+++ base_controllers/__init__.py
@@ -4,3 +4,6 @@ import numpy as np
 np.set_printoptions(threshold=np.inf, precision = 5, linewidth = 10000, suppress = True)
 #do not create .pyc files
 sys.dont_write_bytecode = True
+# for new versions of pycharm
+import matplotlib
+matplotlib.use('TkAgg')
