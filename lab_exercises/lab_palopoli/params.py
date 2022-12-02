c6791efe0842e423a230341a3f8a1678691fb25a
Michele Focchi <michele.focchi@iit.it>
2022-12-02 16:31:01 +0100

    implemented controller_manager for L8 and ur5generic that hides the complexity of the torque position control types

--- lab_exercises/lab_palopoli/params.py
+++ lab_exercises/lab_palopoli/params.py
@@ -23,8 +23,7 @@ robot_params['myrobot'] ={'dt': 0.001,
 robot_params['ur5'] ={'dt': 0.001,
                        'kp': np.array([300, 300, 300,30,30,1]),
                        'kd':  np.array([20,20,20,5, 5,0.5]),
-                       #'q_0':  np.array([ 0.3, -1.3, 1.0, -0.7, 0.7, 0.5]), #limits([0,pi],   [0, -pi], [-pi/2,pi/2],)
-                       'q_0':  np.array([ -0.3223527113543909,-0.7805794638446351, -2.5675506591796875,-1.6347843609251917, -1.5715253988849085, -1.0017417112933558]), #limits([0,pi],   [0, -pi], [-pi/2,pi/2],)
+                       'q_0':  np.array([ -0.32,-0.78, -2.56,-1.63, -1.57, -1.0]), #limits([0,pi],   [0, -pi], [-pi/2,pi/2],)
                        'joint_names': ['shoulder_pan_joint', 'shoulder_lift_joint', 'elbow_joint', 'wrist_1_joint', 'wrist_2_joint', 'wrist_3_joint'],
                        'ee_frame': 'tool0',
                        'control_mode': 'point', # 'trajectory','point'
@@ -33,10 +32,8 @@ robot_params['ur5'] ={'dt': 0.001,
                        'gripper_sim': False, # False: the gripper is treated as a Rigid Body, True: you can move the finger joints
                        'spawn_x' : 0.5,
                        'spawn_y' : 0.35,
-                       'spawn_z' : 1.75,
-                       'buffer_size': 6000} # note the frames are all aligned with base for joints = 0
+                       'spawn_z' : 1.75}
 
-verbose = False
-plotting = False
+plotting = True
 
 
