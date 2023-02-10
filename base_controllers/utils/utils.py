234705acc1224cc261af1f3e094b956574f6762a
froscia <francesco.roscia@iit.it>
2023-02-10 13:34:52 +0100

    bugfix. return value was not correct

--- base_controllers/utils/utils.py
+++ base_controllers/utils/utils.py
@@ -207,13 +207,12 @@ class Utils:
         # create a list of length independent np.ndarrays of shape (rows, cols) with all the entries set to value
         # e.g. full_array_list(2, 1, 2, 5) returns
         # [ array([[5], [5]]), array([[5], [5]])]
-        L = []
         if cols == 0:
             a = np.full(rows, value)
         else:
             a = np.full((rows, cols), value)
-        self.listOfArrays(length, a)
-        return L
+        return self.listOfArrays(length, a)
+
 
     def listOfArrays(self, length, array):
         # create a list of length independent np.ndarrays
