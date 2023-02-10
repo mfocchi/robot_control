0c787f5aadf86f74a1263a5faf6c1d5c38e83282
froscia <francesco.roscia@iit.it>
2023-02-10 11:41:36 +0100

    estimate bias only if acceleration is not nan
    in the first instants IMU_accelerometer is nan, causing
    IMU_accelerometer_bias to be nan forever

--- base_controllers/components/imu_utils.py
+++ base_controllers/components/imu_utils.py
@@ -17,8 +17,10 @@ class IMU_utils:
 
 
     def IMU_bias_estimation(self, b_R_w, IMU_accelerometer):
-            self.IMU_accelerometer_bias = self.alpha_accelerometer * self.IMU_accelerometer_bias + \
-                    (1 - self.alpha_accelerometer) * (IMU_accelerometer - b_R_w @ self.g0)
+        if any(np.isnan(IMU_accelerometer)):
+            return
+        self.IMU_accelerometer_bias = self.alpha_accelerometer * self.IMU_accelerometer_bias + \
+                (1 - self.alpha_accelerometer) * (IMU_accelerometer - b_R_w @ self.g0)
         self.IMU_accelerometer_bias_log[:, self.counter] = self.IMU_accelerometer_bias
         self.counter += 1
 
