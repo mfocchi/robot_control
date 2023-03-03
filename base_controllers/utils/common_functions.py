b6626316c2dd4dd1ecd57a352a9d9958847e3d8b
froscia <francesco.roscia@iit.it>
2023-03-03 14:52:06 +0000

    plotComLinear replaced with plotFrameLinear. added plotFrameAngular. same usage of plotFrame. if you pass a 6d vector it reshape it takes the correct part of the vector you need

--- base_controllers/utils/common_functions.py
+++ base_controllers/utils/common_functions.py
@@ -471,36 +471,234 @@ def plotFrame(name, time_log, des_Pose_log=None, Pose_log=None, des_Twist_log=No
 
     return fig
 
+def plotFrameLinear(name, time_log, des_Pose_log=None, Pose_log=None, des_Twist_log=None, Twist_log=None, des_Acc=None, Acc=None,
+              des_Wrench=None, Wrench_log=None, title=None, frame=None, sharex=True, sharey=True, start=0, end=-1):
+    plot_var_log = None
+    plot_var_des_log = None
+    if name == 'position':
+        labels = ["x", "y", "z"]
+        lin_unit = '[m]'
+        if Pose_log is not None:
+            if Pose_log.shape[0] == 6:
+                plot_var_log = u.linPart(Pose_log)
+            elif Pose_log.shape[0] == 3:
+                plot_var_log = Pose_log
+        if (des_Pose_log is not None):
+            if des_Pose_log.shape[0] == 6:
+                plot_var_des_log = u.linPart(des_Pose_log)
+            elif des_Pose_log.shape[0] == 3:
+                plot_var_des_log = Pose_log
 
+    elif name == 'velocity':
+        labels = ["x", "y", "z"]
+        lin_unit = '[m/s]'
+        if Twist_log is not None:
+            if Twist_log.shape[0] == 6:
+                plot_var_log = u.linPart(Twist_log)
+            elif Twist_log.shape[0] == 3:
+                plot_var_log = Twist_log
+        if (des_Twist_log is not None):
+            if des_Twist_log.shape[0] == 6:
+                plot_var_des_log = u.linPart(des_Twist_log)
+            elif des_Twist_log.shape[0] == 3:
+                plot_var_des_log = Twist_log
 
-def plotCoMLinear(name, figure_id, time_log, plot_var_des_log=None, plot_var_log=None):
-    # neet to transpose the matrix other wise it cannot be plot with numpy array
+    elif name == 'acceleration':
+        labels = ["x", "y", "z"]
+        lin_unit = '[m/s^2]'
+        if Acc is not None:
+            if Acc.shape[0] == 6:
+                plot_var_log = u.linPart(Acc)
+            elif Acc.shape[0] == 3:
+                plot_var_log = Acc
+        if (des_Acc is not None):
+            if des_Acc.shape[0] == 6:
+                plot_var_des_log = u.linPart(des_Acc)
+            elif des_Acc.shape[0] == 3:
+                plot_var_des_log = Acc
+
+    elif name == 'wrench':
+        labels = ["FX", "FY", "FZ"]
+        lin_unit = '[N]'
+        if Wrench_log is not None:
+            if Wrench_log.shape[0] == 6:
+                plot_var_log = u.linPart(Wrench_log)
+            elif Wrench_log.shape[0] == 3:
+                plot_var_log = Wrench_log
+        if (des_Wrench_log is not None):
+            if des_Wrench_log.shape[0] == 6:
+                plot_var_des_log = u.linPart(des_Wrench_log)
+            elif des_Wrench_log.shape[0] == 3:
+                plot_var_des_log = des_Wrench_log
+    else:
+       print("wrong choice")
+
+    if title is None:
+        title = name
+    else:
+        title = title + ' ' + name
+    if frame is not None:
+        title+= ' ' + frame
+
+    dt = time_log[1] - time_log[0]
+    if type(start) == str:
+        start = max(0, int(float(start) / dt + 1))
+    if type(end) == str:
+        end = min(int(float(end) / dt + 1), time_log.shape[0])
+
+    if len(plt.get_fignums()) == 0:
+        figure_id = 1
+    else:
+        figure_id = max(plt.get_fignums()) + 1
     fig = plt.figure(figure_id)
-    fig.suptitle(name, fontsize=20)
-    plt.subplot(3, 1, 1)
-    plt.ylabel("X")
-    plt.plot(time_log, plot_var_log[0, :], linestyle='-', marker="o", markersize=marker_size, lw=lw_act, color='blue')
+    fig.suptitle(title, fontsize=20)
+    ax = subplot(3, 1, 1, sharex=False, sharey=False, ax_to_share=None)
+    plt.ylabel(labels[0] + " "+lin_unit)
     if (plot_var_des_log is not None):
-        plt.plot(time_log, plot_var_des_log[0, :], linestyle='-', marker="o", markersize=marker_size, lw=lw_des,
-                 color='red')
-
+        plt.plot(time_log[start:end], plot_var_des_log[0, start:end], linestyle='-', marker="o", markersize=marker_size, lw=lw_des, color='red')
+    if plot_var_log is not None:
+        plt.plot(time_log[start:end], plot_var_log[0, start:end], linestyle='-', marker="o", markersize=marker_size, lw=lw_act, color='blue')
     plt.grid()
 
-    plt.subplot(3, 1, 2)
-    plt.ylabel("Y")
-    plt.plot(time_log, plot_var_log[1, :], linestyle='-', marker="o", markersize=marker_size, lw=lw_act, color='blue',
-             label="q")
+    subplot(3, 1, 2, sharex=sharex, sharey=sharey, ax_to_share=ax)
+    plt.ylabel(labels[1] + " "+lin_unit)
     if (plot_var_des_log is not None):
-        plt.plot(time_log, plot_var_des_log[1, :], linestyle='-', lw=lw_des, color='red', label="des")
+       plt.plot(time_log[start:end], plot_var_des_log[1, start:end], linestyle='-', lw=lw_des, color='red')
+    if plot_var_log is not None:
+        plt.plot(time_log[start:end], plot_var_log[1, start:end], linestyle='-', marker="o", markersize=marker_size, lw=lw_act,
+            color='blue')
     plt.grid()
 
-    plt.subplot(3, 1, 3)
-    plt.ylabel("Z")
-    plt.plot(time_log, plot_var_log[2, :], linestyle='-', marker="o", markersize=marker_size, lw=lw_act, color='blue')
+    subplot(3, 1, 3, sharex=sharex, sharey=sharey, ax_to_share=ax)
+    plt.ylabel(labels[2] + " "+lin_unit)
+    plt.xlabel("Time [s]")
     if (plot_var_des_log is not None):
-        plt.plot(time_log, plot_var_des_log[2, :], linestyle='-', lw=lw_des, color='red')
+       plt.plot(time_log[start:end], plot_var_des_log[2, start:end], linestyle='-', lw=lw_des, color='red')
+    if plot_var_log is not None:
+        plt.plot(time_log[start:end], plot_var_log[2, start:end], linestyle='-', marker="o", markersize=marker_size, lw=lw_act,
+            color='blue')
     plt.grid()
 
+    fig.align_ylabels(fig.axes[:3])
+
+    return fig
+
+
+def plotFrameAngular(name, time_log, des_Pose_log=None, Pose_log=None, des_Twist_log=None, Twist_log=None, des_Acc=None, Acc=None,
+                    des_Wrench_log=None, Wrench_log=None, title=None, frame=None, sharex=True, sharey=True, start=0, end=-1):
+    plot_var_log = None
+    plot_var_des_log = None
+    if name == 'position':
+        labels = ["R", "P", "Y"]
+        ang_unit = '[rad]'
+        if Pose_log is not None:
+            if Pose_log.shape[0] == 6:
+                plot_var_log = u.angPart(Pose_log)
+            elif Pose_log.shape[0] == 3:
+                plot_var_log = Pose_log
+        if (des_Pose_log is not None):
+            if des_Pose_log.shape[0] == 6:
+                plot_var_des_log = u.angPart(des_Pose_log)
+            elif des_Pose_log.shape[0] == 3:
+                plot_var_des_log = Pose_log
+
+    elif name == 'velocity':
+        labels = ["R", "P", "Y"]
+        ang_unit = '[rad]'
+        if Twist_log is not None:
+            if Twist_log.shape[0] == 6:
+                plot_var_log = u.angPart(Twist_log)
+            elif Twist_log.shape[0] == 3:
+                plot_var_log = Twist_log
+        if (des_Twist_log is not None):
+            if des_Twist_log.shape[0] == 6:
+                plot_var_des_log = u.angPart(des_Twist_log)
+            elif des_Twist_log.shape[0] == 3:
+                plot_var_des_log = Twist_log
+
+    elif name == 'acceleration':
+        labels = ["R", "P", "Y"]
+        ang_unit = '[rad]'
+        if Acc is not None:
+            if Acc.shape[0] == 6:
+                plot_var_log = u.angPart(Acc)
+            elif Acc.shape[0] == 3:
+                plot_var_log = Acc
+        if (des_Acc is not None):
+            if des_Acc.shape[0] == 6:
+                plot_var_des_log = u.angPart(des_Acc)
+            elif des_Acc.shape[0] == 3:
+                plot_var_des_log = Acc
+
+    elif name == 'wrench':
+        labels = ["MX", "MY", "MZ"]
+        ang_unit = '[Nm]'
+        if Wrench_log is not None:
+            if Wrench_log.shape[0] == 6:
+                plot_var_log = u.angPart(Wrench_log)
+            elif Wrench_log.shape[0] == 3:
+                plot_var_log = Wrench_log
+        if (des_Wrench_log is not None):
+            if des_Wrench_log.shape[0] == 6:
+                plot_var_des_log = u.angPart(des_Wrench_log)
+            elif des_Wrench_log.shape[0] == 3:
+                plot_var_des_log = des_Wrench_log
+    else:
+        print("wrong choice")
+
+    if title is None:
+        title = name
+    else:
+        title = title + ' ' + name
+    if frame is not None:
+        title += ' ' + frame
+
+    dt = time_log[1] - time_log[0]
+    if type(start) == str:
+        start = max(0, int(float(start) / dt + 1))
+    if type(end) == str:
+        end = min(int(float(end) / dt + 1), time_log.shape[0])
+
+    if len(plt.get_fignums()) == 0:
+        figure_id = 1
+    else:
+        figure_id = max(plt.get_fignums()) + 1
+    fig = plt.figure(figure_id)
+    fig.suptitle(title, fontsize=20)
+    ax = subplot(3, 1, 1, sharex=False, sharey=False, ax_to_share=None)
+    plt.ylabel(labels[0] + " " + ang_unit)
+    if (plot_var_des_log is not None):
+        plt.plot(time_log[start:end], plot_var_des_log[0, start:end], linestyle='-', marker="o", markersize=marker_size,
+                 lw=lw_des, color='red')
+    if plot_var_log is not None:
+        plt.plot(time_log[start:end], plot_var_log[0, start:end], linestyle='-', marker="o", markersize=marker_size,
+                 lw=lw_act, color='blue')
+    plt.grid()
+
+    subplot(3, 1, 2, sharex=sharex, sharey=sharey, ax_to_share=ax)
+    plt.ylabel(labels[1] + " " + ang_unit)
+    if (plot_var_des_log is not None):
+        plt.plot(time_log[start:end], plot_var_des_log[1, start:end], linestyle='-', lw=lw_des, color='red')
+    if plot_var_log is not None:
+        plt.plot(time_log[start:end], plot_var_log[1, start:end], linestyle='-', marker="o", markersize=marker_size,
+                 lw=lw_act,
+                 color='blue')
+    plt.grid()
+
+    subplot(3, 1, 3, sharex=sharex, sharey=sharey, ax_to_share=ax)
+    plt.ylabel(labels[2] + " " + ang_unit)
+    plt.xlabel("Time [s]")
+    if (plot_var_des_log is not None):
+        plt.plot(time_log[start:end], plot_var_des_log[2, start:end], linestyle='-', lw=lw_des, color='red')
+    if plot_var_log is not None:
+        plt.plot(time_log[start:end], plot_var_log[2, start:end], linestyle='-', marker="o", markersize=marker_size,
+                 lw=lw_act,
+                 color='blue')
+    plt.grid()
+
+    fig.align_ylabels(fig.axes[:3])
+
+    return fig
 
 
 def plotGRFs(figure_id, time_log, des_forces, act_forces):
