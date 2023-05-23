caa031f79d899c023e63508e92329f5ef6b668a7
Michele Focchi <michele.focchi@iit.it>
2023-05-23 16:23:14 +0200

    tractor works with coppeliasim

--- lab_exercises/lab_palopoli/tractor_simulator.py
+++ lab_exercises/lab_palopoli/tractor_simulator.py
@@ -10,11 +10,14 @@ import rospy as ros
 from base_controllers.utils.math_tools import *
 np.set_printoptions(threshold=np.inf, precision = 5, linewidth = 1000, suppress = True)
 from base_controllers.base_controller import BaseController
-from base_controllers.utils.common_functions import plotFrame, plotJoint
-
+from base_controllers.utils.common_functions import checkRosMaster, plotFrame, plotJoint
+import time
 import params as conf
 robotName = "tractor" # needs to inherit BaseController
-
+import os
+from std_msgs.msg import Bool, Int32, Float32
+import rospkg
+import roslaunch
 
 class GenericSimulator(BaseController):
     
@@ -22,7 +25,7 @@ class GenericSimulator(BaseController):
         super().__init__(robot_name=robot_name, external_conf = conf)
         self.freezeBaseFlag = False
         self.torque_control = False
-        print("Initialized murobot controller---------------------------------------------------------------")
+        print("Initialized tractor controller---------------------------------------------------------------")
 
     def initVars(self):
         super().initVars()
@@ -35,30 +38,92 @@ class GenericSimulator(BaseController):
                 pass
             super().logData()
 
+    def startSimulator(self):
+        # clean up previous process
+        os.system("killall rosmaster coppeliaSim")
+        # launch roscore
+        checkRosMaster()
+        ros.sleep(0.5)
+
+        # launch coppeliasim
+        scene = rospkg.RosPack().get_path('tractor_description') + '/CoppeliaSimModels/scene.ttt'
+        file = os.getenv("LOCOSIM_DIR")+"/CoppeliaSim/coppeliaSim.sh"+" "+scene+" &"
+        os.system(file)
+        ros.sleep(1.5)
+
+        # run robot state publisher
+        package = 'robot_state_publisher'
+        executable = 'robot_state_publisher'
+        node = roslaunch.core.Node(package, executable)
+        launch = roslaunch.scriptapi.ROSLaunch()
+        launch.start()
+        process = launch.launch(node)
+
+        # run rviz
+        package = 'rviz'
+        executable = 'rviz'
+        args = '-d ' + rospkg.RosPack().get_path('ros_impedance_controller') + '/config/operator.rviz'
+        node = roslaunch.core.Node(package, executable, args=args)
+        launch = roslaunch.scriptapi.ROSLaunch()
+        launch.start()
+        process = launch.launch(node)
+
+    def loadModelAndPublishers(self):
+        super().loadModelAndPublishers()
+        self.startPub=ros.Publisher("/startSimulation", Bool, queue_size=1, tcp_nodelay=True)
+        self.pausePub=ros.Publisher("/pauseSimulation", Bool, queue_size=1, tcp_nodelay=True)
+        self.stopPub=ros.Publisher("/stopSimulation", Bool, queue_size=1, tcp_nodelay=True)
+        self.enableSynModePub=ros.Publisher("/enableSyncMode", Bool, queue_size=1, tcp_nodelay=True)
+        self.triggerNextStepPub=ros.Publisher("/triggerNextStep", Bool, queue_size=1, tcp_nodelay=True)
+        # simStepDoneSub=ros.Subscriber("/simulationStepDone", Bool, sim_done_)
+        self.simStateSub=ros.Subscriber("/simulationState", Int32, self.sim_state_)
+        # simTimeSub=simROS.Subscriber('/simulationTime',Float32, time_sim_)
+
+    def sim_state_(self, msg):
+        pass
+
+    def deregister_node(self):
+        self.stopPub.publish(True)
+        os.system("killall rosmaster coppeliaSim")
+        super().deregister_node()
+
+    def simulationControl(self,input):
+        if input == 'start':
+            print("starting simulation")
+            ros.wait_for_message('/simulationState', Bool, timeout=2.)
+            msg = Bool()
+            msg.data = 1
+            self.startPub.publish(msg)
+
 def talker(p):
     p.start()
     additional_args = None #'gui:=false'
     #p.startSimulator(additional_args = ['spawn_Y:=3.14'])
-    p.startSimulator(world_name='tractor.world', additional_args=['spawn_Y:=3.14'])
+    #p.startSimulator(world_name='tractor.world', additional_args=['spawn_Y:=3.14'])
+
+
+
+    p.startSimulator()
+
     p.loadModelAndPublishers()
     p.initVars()
     p.initSubscribers()
-    p.startupProcedure()
+    p.simulationControl('start')
+
     #loop frequency
     rate = ros.Rate(1/conf.robot_params[p.robot_name]['dt'])
 
     p.q_des = np.copy(p.q_des_q0)
     # for torque control
-    if   p.torque_control:
-        p.pid.setPDs(0.0, 0.0, 0.0)
+
     # # equivalent
     # p.pid.setPDjoint(0, 0.0, 0.0, 0.0)
     # p.pid.setPDjoint(1, 0.0, 0.0, 0.0)
-    # p.pid.setPDjoint(2, 0.0, 0.0, 0.0)
-    # p.pid.setPDjoint(3, 0.0, 0.0, 0.0)
+
     forward_speed = -0.007
 
     while not ros.is_shutdown():
+        #p.simulationControl('start')
         p.qd_des = forward_speed * np.ones(4)
         p.q_des = p.q_des + forward_speed*np.ones(4)
         if p.torque_control:
@@ -81,6 +146,8 @@ if __name__ == '__main__':
     except (ros.ROSInterruptException, ros.service.ServiceException):
         ros.signal_shutdown("killed")
         p.deregister_node()
-        plotJoint('position', 0, p.time_log, p.q_log, p.q_des_log, joint_names = p.joint_names)
+        plotJoint('position', p.time_log, p.q_log, p.q_des_log, joint_names = p.joint_names)
+        plotJoint('position', time_log=p.time_log, q_log=p.q_log, q_des_log=p.q_des_log, sharex=True, sharey=False,
+                  start=0, end=-1)
 
 
