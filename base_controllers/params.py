a3dd14997f6433b93ca9338a1fbcd5cb937b26a9
Michele Focchi <michele.focchi@iit.it>
2024-11-07 18:13:12 +0100

    tuned gains

--- base_controllers/params.py
+++ base_controllers/params.py
@@ -161,9 +161,9 @@ robot_params['go1'] ={'dt': 0.002,
                       'kd_ang': np.array([1.51, 1.51, 1.51]),                      
                       # real robot gains
                       # stand alone joint pid
-                      'kp_real': np.array([30., 30.,30.]*4),
-                      'kd_real': np.array([.25, .25, .25]*4),
-                      'ki_real': np.array([2, 2, 2]*4),
+                      'kp_real': np.array([20., 20.,20.]*4),
+                      'kd_real': np.array([.14, .25, .3]*4),
+                      'ki_real': np.array([1, 1, 1]*4),
                        # joint pid + wbc (optional)
                       'kp_wbc_real': np.array([20., 30., 40.]*4),
                       'kd_wbc_real': np.array([.3, .3, .3]*4),
@@ -175,8 +175,8 @@ robot_params['go1'] ={'dt': 0.002,
                       # 'kd_ang_real': np.array([10., 10., 10.]),
                       'kp_lin_real': np.array([300., 300., 450.]), #np.array([300., 300., 400.]), # x y z
                       'kd_lin_real': np.array([40., 40., 60.]), #np.array([30., 20., 60.]),
-                      'kp_ang_real': np.array([20., 20., 20.]), # #np.array([30., 50., 30.]), # R P Y
-                      'kd_ang_real': np.array([1.5, 1.5, 1.5]), #np.array([2., 4., 2.]),
+                      'kp_ang_real': np.array([20., 40., 20.]), # #np.array([30., 50., 30.]), # R P Y
+                      'kd_ang_real': np.array([1.5, 2.5, 1.5]), #np.array([2., 4., 2.]),
                       # joint configuration
                       'q_0':  np.array([0.2, 0.78, -1.7,  0.2, 0.78, -1.7, -0.2, 0.78, -1.7, -0.2, 0.78, -1.7]),
                       #orbit
