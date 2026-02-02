371c67e7d13a4221b228e16511606c33d326eed6
Michele Focchi <michele.focchi@unitn.it>
2026-02-02 19:13:58 +0100

    climbingrobotcontroller2:
    regenerated code test_mex:
    -added patch_side_y/z, r
    -using 2norm in smooth cost  (cost was negative!)
    - tuned cost weights w1 w3
    -mas reverted to 5kg
    -increased FrMax =190

--- base_controllers/climbingrobot_controller/codegen_mesh_landing/mex/optimize_cpp/optimize_cpp_mex.sh
+++
@@ -1,2 +0,0 @@
-"/usr/local/MATLAB/R2023a/toolbox/shared/coder/ninja/glnxa64/ninja" -t compdb cc cxx cudac > compile_commands.json
-"/usr/local/MATLAB/R2023a/toolbox/shared/coder/ninja/glnxa64/ninja" -v "$@"
