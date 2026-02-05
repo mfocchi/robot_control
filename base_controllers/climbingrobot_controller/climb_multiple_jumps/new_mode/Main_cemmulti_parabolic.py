1a74d7451dd72b106b75767f763803bdbe711a8c
Ruben_Malacarne <ruben.malacarne@gmail.com>
2026-02-05 11:59:54 +0100

    fix some bugs now starting to do some test

--- base_controllers/climbingrobot_controller/climb_multiple_jumps/new_mode/Main_cemmulti_parabolic.py
+++ base_controllers/climbingrobot_controller/climb_multiple_jumps/new_mode/Main_cemmulti_parabolic.py
@@ -20,7 +20,7 @@ def main():
     setting = {
         "COMPUTATION_MODE": True,
         "WARM_START_MODE": False,
-        "PLOT_MODE": True,
+        "PLOT_MODE": False,
         "EARLY_STOP": True
     }
     
@@ -340,14 +340,13 @@ def main():
         
         save_gazebo_info(best_jump_log_points, terrain_manager)
         
+    if setting["PLOT_MODE"]:
         if best_jump_log_points and best_trajectory:
             optimizer.plot_point_traj(best_jump_log_points, best_trajectory)
             optimizer.plot_mesh_traj(best_jump_log_points, best_trajectory,best_fitness)
         else:
             print(colored("[ERROR] Could not plot best trajectory. No solution found or tracking issue.", "red", attrs=['bold']))
-    if setting["PLOT_MODE"]:
-        
-        print("plot da stampare")
+            
 if __name__ == "__main__":
     # try:
         main()
