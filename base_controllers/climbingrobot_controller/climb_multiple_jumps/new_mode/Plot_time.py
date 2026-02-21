e8a54f0084a0250788bb12010a56f926cb8d4491
Ruben_Malacarne <ruben.malacarne@gmail.com>
2026-02-21 09:56:34 +0100

    add combine plot too see the result of different test togheter

--- base_controllers/climbingrobot_controller/climb_multiple_jumps/new_mode/Plot_time.py
+++ base_controllers/climbingrobot_controller/climb_multiple_jumps/new_mode/Plot_time.py
@@ -13,7 +13,7 @@ plot_str = os.environ.get("FOLDER_PLOT")
 if plot_str:
     FOLDER_MAIN = np.array(json.loads(plot_str))
 else:
-    FOLDER_MAIN = "result/test_final_v1_5_gaussian_bumps"
+    FOLDER_MAIN = "result/test_r_0_hemisphere"
 
 TIMING_FILE = os.path.join(FOLDER_MAIN, "timing_report.json")
 
